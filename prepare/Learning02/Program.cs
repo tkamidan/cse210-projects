7c59ffeca7212ffbf673c82f20a46046fd5a298e
tkamidan <tkamidan@outlook.com>
2023-05-02 09:44:35 -0600

    Created the program

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2015;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Graphic Designer";
+        job2._company = "Nintendo";
+        job2._startYear = 2022;
+        job2._endYear = 2024;
+
+        job1.Display();
+        job2.Display();
+
+        Resume ta = new Resume();
+        ta._name = "Treven Amidan";
+        ta._jobs.Add(job1);
+        ta._jobs.Add(job2);
+        Console.WriteLine(ta._jobs[0]._jobTitle);
+
+        ta.Display();
     }
 }
