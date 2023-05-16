fba90f1f159489064b30aa61c0c4d3a70b4e1f0e
tkamidan <tkamidan@outlook.com>
2023-05-16 09:38:07 -0600

    Created Program

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction num1 = new Fraction();
+        Fraction num2 = new Fraction(10);
+        Fraction num3 = new Fraction(2, 3);
+
+        Console.WriteLine(num1.GetFractionString());
+        Console.WriteLine(num1.GetDecimalValue());
+        Console.WriteLine(num2.GetFractionString());
+        Console.WriteLine(num2.GetDecimalValue());
+        Console.WriteLine(num3.GetFractionString());
+        Console.WriteLine(num3.GetDecimalValue());
     }
 }
