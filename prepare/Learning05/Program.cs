51c51242f392164dff580cee3e36f73c7c89616a
tkamidan <tkamidan@outlook.com>
2023-06-13 18:11:52 -0600

    Created the Program

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("Blue", 5);
+        Console.WriteLine(square.GetColor());
+        Console.WriteLine(square.GetArea());
+
+        Rectangle rectangle = new Rectangle("Red", 5, 10);
+        Console.WriteLine(rectangle.GetColor());
+        Console.WriteLine(rectangle.GetArea());
+
+        Circle circle = new Circle("Pink", 5);
+        Console.WriteLine(circle.GetColor());
+        Console.WriteLine(circle.GetArea());
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("Green", 6));
+        shapes.Add(new Rectangle("Brown", 4, 6));
+        shapes.Add(new Circle("Black", 9));
+
+        foreach(Shape shape in shapes) {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
